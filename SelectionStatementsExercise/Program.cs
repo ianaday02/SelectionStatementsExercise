2bdf2f448b007f219c86703937177c217735b44b
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-09 21:54:31 -0500

    initial commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 555;
+
+            Console.WriteLine("Try to guess my favorite number!");
+            var userInput = int.Parse(Console.ReadLine());
+           
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
+
